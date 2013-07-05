400516d902b9c12088ffa4126c3d18a52fa5e49e
falsandtru <falsandtru>
2013-07-05 18:02:47 +0900

    update to 1.14.0

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -5,8 +5,8 @@
  * ---
  * @Copyright(c) 2012, falsandtru
  * @license MIT  http://opensource.org/licenses/mit-license.php  http://sourceforge.jp/projects/opensource/wiki/licenses%2FMIT_license
- * @version 1.13.4
- * @updated 2013/07/03
+ * @version 1.14.0
+ * @updated 2013/07/05
  * @author falsandtru  http://fat.main.jp/  http://sa-kusaku.sakura.ne.jp/
  * @CodingConventions Google JavaScript Style Guide
  * ---
@@ -122,7 +122,8 @@
         landing : win.location.href ,
         validate : validate ,
         retry : true ,
-        speed : { now : function () { return ( new Date() ).getTime() ; } }
+        speed : { now : function () { return ( new Date() ).getTime() ; } } ,
+        options : options
       }
     ) ;
     
@@ -139,9 +140,7 @@
     /* Function declaration */
     
     function check() {
-      if ( !supportPushState() ) { return false ; } ;
-      if ( !jQuery( settings.area ).length ) { return false ; }
-      return true ;
+      return supportPushState() ;
     } // function: check
     
     function supportPushState() {
@@ -285,10 +284,15 @@
       
       for ( var i in callbacks ) {
         if ( i in settings.callbacks.ajax || i === 'xhr' ) { continue ; } ;
-        delete callbacks[ i ] ;
+        callbacks[ i ] = undefined ;
       } ;
       
-      /* validate */ validate && validate.test( 4, 1, 0, 'drive:popstate' ) ;
+      /* validate */ validate && validate.test( 4, 1, 0, 'drive:area' ) ;
+      AREA : {
+        settings.area = typeof settings.options.area === 'function' ? settings.options.area.apply( null , [ event ] ) : settings.options.area ;
+      } ;
+      
+      /* validate */ validate && validate.test( 5, 1, 0, 'drive:popstate' ) ;
       POPSTATE : {
         if ( event.type.toLowerCase() !== 'popstate' ) { break POPSTATE ; } ;
         
@@ -301,7 +305,7 @@
         settings.ajax.data = null ;
       } ;
       
-      /* validate */ validate && validate.test( 5, 1, 0, 'drive:submit' ) ;
+      /* validate */ validate && validate.test( 6, 1, 0, 'drive:submit' ) ;
       SUBMIT : {
         if ( event.type.toLowerCase() !== 'submit' ) { break SUBMIT ;} ;
         
@@ -311,7 +315,7 @@
         GET : {
           if ( settings.ajax.type !== 'GET' ) { break GET ;} ;
           
-          /* validate */ validate && validate.test( 5.1, 1, jQuery( event.target ).serialize(), 'drive:serialize' ) ;
+          /* validate */ validate && validate.test( 6.1, 1, jQuery( event.target ).serialize(), 'drive:serialize' ) ;
           url += '?' + jQuery( event.target ).serialize() ;
           settings.ajax.data = null ;
         } ;
@@ -319,24 +323,24 @@
         POST : {
           if ( settings.ajax.type !== 'POST' ) { break POST ;} ;
           
-          /* validate */ validate && validate.test( 5.1, 1, jQuery( event.target ).serializeArray(), 'drive:serializeArray' ) ;
+          /* validate */ validate && validate.test( 6.1, 1, jQuery( event.target ).serializeArray(), 'drive:serializeArray' ) ;
           settings.ajax.data = jQuery( event.target ).serializeArray() ;
         } ;
       } ;
       
-      /* validate */ validate && validate.test( 6, 1, 0, 'drive:url' ) ;
+      /* validate */ validate && validate.test( 7, 1, 0, 'drive:url' ) ;
       URL : {
         if ( !settings.server.query ) { break URL ; } ;
         url = url.replace( /(#[^\s]*|)$/ , ( !url.match( /\?/ ) ? '?' :'&' ) + encodeURIComponent( settings.server.query ) + '=1' + '$1' ) ;
       } ;
       
-      /* validate */ validate && validate.test( 7, 1, 0, 'drive:ajax' ) ;
+      /* validate */ validate && validate.test( 8, 1, 0, 'drive:ajax' ) ;
       settings.speedcheck && settings.log.speed.name.push( 'ajax_start' ) ;
       settings.speedcheck && settings.log.speed.time.push( settings.speed.now() - settings.log.speed.fire ) ;
       jQuery.when ? ajax_regular() : ajax_legacy() ;
       
       if ( fire( settings.callbacks.after , context , [ event , settings.parameter ] , settings.callbacks.async ) === false ) { return ; } ; // function: drive
-      /* validate */ validate && validate.test( 8, 1, 0, 'drive:end' ) ;
+      /* validate */ validate && validate.test( 9, 1, 0, 'drive:end' ) ;
       /* validate */ validate && validate.end() ;
       
       
