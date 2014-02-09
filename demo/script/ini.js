564e9080c25b42d0b92c02777bffc711affe68c3
falsandtru <falsandtru>
2014-02-09 13:10:41 +0900

    Add: demo

---
+++ demo/script/ini.js
@@ -0,0 +1,11 @@
+$(function()
+{
+  $.pjax({
+    area: 'div.pjax' ,
+    load:
+    {
+      script: true
+    }
+  });
+  alert( 'execute script [linked(head)]\n' + $('div.primary p:first-child').text() )
+});
