4f68b7187aa71576a63a08b9abee9ec1f113e33d
falsandtru <falsandtru>
2014-02-13 23:25:56 +0900

    Fix: database()

--- jquery.pjax.dev.js
+++ jquery.pjax.dev.js
@@ -1181,7 +1181,7 @@
         db = idb.open( name ) ;
         /* validator */ validator && validator.test( '++', 1, 0, 'call' ) ;
         db.onblocked = function () {
-          /* validator */ validator && validator.test( '++', 0, 0, 'onblocked()' ) ;
+          /* validator */ validator && validator.test( '++', 1, 0, 'onblocked()' ) ;
         } ;
         db.onupgradeneeded = function () {
           /* validator */ validator && validator.test( '++', 1, 0, 'onupgradeneeded()' ) ;
@@ -1222,11 +1222,13 @@
             setting.database = false ;
             db.close() ;
             idb.deleteDatabase( name ) ;
+            setTimeout( function () { Store.database( ++count ) ; }, 1000 ) ;
           }
           /* validator */ validator && validator.end() ;
         } ;
         db.onerror = function ( event ) {
           /* validator */ validator && validator.test( '++', 1, event, 'onerror()' ) ;
+          db.close() ;
           idb.deleteDatabase( name ) ;
           setTimeout( function () { Store.database( ++count ) ; }, 1000 ) ;
           /* validator */ validator && validator.end() ;
