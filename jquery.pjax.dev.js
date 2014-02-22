85ccee90e18ec956fa21dd551fb906a484c1a5da
falsandtru <falsandtru>
2014-02-23 07:12:38 +0900

    Version: 1.32.1

--- jquery.pjax.dev.js
+++ jquery.pjax.dev.js
@@ -5,8 +5,8 @@
  * ---
  * @Copyright(c) 2012, falsandtru
  * @license MIT http://opensource.org/licenses/mit-license.php
- * @version 1.32.0
- * @updated 2014/02/22
+ * @version 1.32.1
+ * @updated 2014/02/23
  * @author falsandtru https://github.com/falsandtru/
  * @CodingConventions Google JavaScript Style Guide
  * ---
