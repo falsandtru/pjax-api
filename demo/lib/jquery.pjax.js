72df1ef4eb90570616cdc494c866bc1c274a8097
falsandtru <falsandtru>
2014-06-01 23:31:02 +0900

    Version: 1.33.0

--- demo/lib/jquery.pjax.js
+++ demo/lib/jquery.pjax.js
@@ -5,8 +5,8 @@
  * ---
  * @Copyright(c) 2012, falsandtru
  * @license MIT http://opensource.org/licenses/mit-license.php
- * @version 1.32.9
- * @updated 2014/04/22
+ * @version 1.33.0
+ * @updated 2014/06/01
  * @author falsandtru https://github.com/falsandtru/
  * @CodingConventions Google JavaScript Style Guide
  * ---
