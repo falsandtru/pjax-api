34f4ebc578f55faa0c1230fadb12c3f959d9a45d
falsandtru <falsandtru>
2014-07-17 23:35:18 +0900

    Update source definition file

--- src/ts/define.ts
+++ src/ts/define.ts
@@ -1,4 +1,5 @@
 /// <reference path=".d/jquery.d.ts"/>
+/// <reference path=".d/jquery.pjax.d.ts"/>
 
 interface Window {
   DOMParser?: any
@@ -16,8 +17,8 @@ interface JQueryXHR {
 }
 module MODULE {
 
-  export var NAME: string = 'pjax';
-  export var NAMESPACE: any = jQuery;
+  export var NAME: string = 'pjax'
+  export var NAMESPACE: any = jQuery
 
   /*
    * 仕様
@@ -216,8 +217,6 @@ module MODULE {
   }
   export interface SettingInterface {
     // public
-    gns: string
-    ns: string
     area: any
     link: string
     filter(): boolean
@@ -243,7 +242,72 @@ module MODULE {
       }
     }
     callback(): any
-    callbacks: any
+    callbacks: {
+      before?: (event: JQueryEventObject, param: any) => any
+      after?: (event: JQueryEventObject, param: any) => any
+      ajax: {
+        xhr?: (event: JQueryEventObject, param: any) => any
+        beforeSend?: (event: JQueryEventObject, param: any, data: string, ajaxSettings: any) => any
+        dataFilter?: (event: JQueryEventObject, param: any, data: string, dataType: any) => any
+        success?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        error?: (event: JQueryEventObject, param: any, XMLHttpRequest: XMLHttpRequest, textStatus: string, errorThrown: any) => any
+        complete?: (event: JQueryEventObject, param: any, XMLHttpRequest: XMLHttpRequest, textStatus: string) => any
+        done?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        fail?: (event: JQueryEventObject, param: any, XMLHttpRequest: XMLHttpRequest, textStatus: string, errorThrown: any) => any
+        always?: (event: JQueryEventObject, param: any, XMLHttpRequest: XMLHttpRequest, textStatus: string) => any
+      }
+      update: {
+        before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        cache: {
+          before?: (event: JQueryEventObject, param: any, cache: any) => any
+          after?: (event: JQueryEventObject, param: any, cache: any) => any
+        }
+        redirect: {
+          before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+          after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        }
+        url: {
+          before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+          after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        }
+        title: {
+          before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+          after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        }
+        head: {
+          before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+          after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        }
+        content: {
+          before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+          after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        }
+        scroll: {
+          before?: (event: JQueryEventObject, param: any) => any
+          after?: (event: JQueryEventObject, param: any) => any
+        }
+        css: {
+          before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+          after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        }
+        script: {
+          before?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+          after?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        }
+        render: {
+          before?: (event: JQueryEventObject, param: any) => any
+          after?: (event: JQueryEventObject, param: any) => any
+        }
+        verify: {
+          before?: (event: JQueryEventObject, param: any) => any
+          after?: (event: JQueryEventObject, param: any) => any
+        }
+        success?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        error?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+        complete?: (event: JQueryEventObject, param: any, data: string, textStatus: string, XMLHttpRequest: XMLHttpRequest) => any
+      }
+    }
     param: any
     load: {
       css: boolean
@@ -284,6 +348,8 @@ module MODULE {
     
     // internal
     uuid: string
+    gns: string
+    ns: string
     nss: {
       name: string
       event: string[]
