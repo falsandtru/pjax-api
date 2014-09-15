1a1efa4ac0b9f42831648801ebd7583e80c38078
falsandtru <falsandtru>
2014-09-16 01:44:04 +0900

    Improve HTML Parser

--- src/ts/model/app.page.update.ts
+++ src/ts/model/app.page.update.ts
@@ -7,8 +7,6 @@
 
 module MODULE.MODEL {
 
-  setTimeout(() => AppPageUpdate.createHTMLDocument_('', ''), 50);
-
   export class AppPageUpdate extends AppPageUtility implements AppPageUpdateInterface {
     
     constructor(
@@ -26,7 +24,6 @@ module MODULE.MODEL {
     public host_: string
     ) {
       super();
-      this.createHTMLDocument_ = AppPageUpdate.createHTMLDocument_;
       this.main_();
     }
 
@@ -69,7 +66,7 @@ module MODULE.MODEL {
           if (!~(jqXHR.getResponseHeader('Content-Type') || '').toLowerCase().search(setting.contentType)) { throw new Error("throw: content-type mismatch"); }
           
           /* variable define */
-          this.srcDocument_ = this.createHTMLDocument_(jqXHR.responseText, setting.destLocation.href);
+          this.srcDocument_ = this.createHTMLDocument(jqXHR.responseText, setting.destLocation.href);
           this.dstDocument_ = document;
             
           // 更新範囲を選出
@@ -304,7 +301,7 @@ module MODULE.MODEL {
 
       if (cache && cache.data) {
         var html: string = setting.fix.noscript ? this.restoreNoscript_(cache.data) : cache.data,
-            cacheDocument: Document = this.createHTMLDocument_(html, setting.destLocation.href),
+            cacheDocument: Document = this.createHTMLDocument(html, setting.destLocation.href),
             srcDocument: Document = this.srcDocument_;
 
         srcDocument.title = cacheDocument.title;
@@ -751,109 +748,6 @@ module MODULE.MODEL {
       jQuery.removeData(script, 'code');
     }
     
-    createHTMLDocument_: (html: string, uri: string) => Document
-
-    static createHTMLDocument_(html: string, uri: string): Document {
-      var mode: string;
-
-      this.createHTMLDocument_ = (html: string, uri: string) => {
-        function test_(conv: (html: string, uri: string) => Document, ...args): boolean {
-          try {
-            var html = '<html lang="en" class="html"><head><title>pjax</title><link href="/"><noscript><style>/**/</style></noscript></head><body><noscript>noscript</noscript><a href="/"></a></body></html>',
-                doc = conv(html, '');
-            return doc &&
-                   doc.URL && decodeURI(doc.URL) === decodeURI(uri || window.location.href) &&
-                   doc.title === 'pjax' &&
-                   doc.querySelector('html.html[lang="en"]') &&
-                   doc.querySelector('head>link')['href'] &&
-                   doc.querySelector('head>noscript')['innerHTML'] &&
-                   doc.querySelector('body>noscript')['innerHTML'] === 'noscript' &&
-                   doc.querySelector('body>a')['href'] &&
-                   true || false;
-          } catch (err) {
-            return false;
-          }
-        };
-        function manipulate(doc: Document, html: string): Document {
-          var wrapper = <HTMLElement>document.createElement('div');
-          wrapper.innerHTML = (html.match(/<html(?:\s.*?[^\\])?>/i) || ['<html>']).shift().replace(/html/i, 'div') + '</div>';
-          var attrs = wrapper.firstChild.attributes;
-          for (var i = 0, attr: Attr; attr = attrs[i]; i++) {
-            doc.documentElement.setAttribute(attr.name, attr.value);
-          }
-          var wrapper = <HTMLElement>document.createElement('html');
-          wrapper.innerHTML = html.replace(/^.*?<html(?:\s.*?[^\\])?>/im, '');
-          doc.documentElement.removeChild(doc.head);
-          doc.documentElement.removeChild(doc.body);
-          while (wrapper.childNodes.length) {
-            doc.documentElement.appendChild(wrapper.childNodes[0]);
-          }
-          return doc;
-        };
-
-        var backup = window.location.href;
-        uri && window.history.replaceState(window.history.state, document.title, uri);
-
-        var doc: Document;
-        switch (mode) {
-          // firefox
-          case 'dom':
-            if ('function' === typeof window.DOMParser) {
-              doc = new window.DOMParser().parseFromString(html, 'text/html');
-            }
-            break;
-
-          // chrome, safari
-          case 'doc':
-            if (document.implementation && document.implementation.createHTMLDocument) {
-              doc = document.implementation.createHTMLDocument('');
-              
-              // IE, Operaクラッシュ対策
-              if ('object' !== typeof doc.activeElement || !doc.activeElement) { break; }
-
-              // titleプロパティの値をChromeで事後に変更できなくなったため事前に設定する必要がある
-              if ('function' === typeof window.DOMParser) {
-                doc.title = new window.DOMParser().parseFromString(html.match(/<title(?:\s.*?[^\\])?>(?:.*?[^\\])?<\/title>/i), 'text/html').title;
-              }
-              doc.open();
-              doc.write(html);
-              doc.close();
-            }
-            break;
-          
-          // ie10+, opera
-          case 'manipulate':
-            if (document.implementation && document.implementation.createHTMLDocument) {
-              doc = manipulate(document.implementation.createHTMLDocument(''), html);
-            }
-            break;
-
-          default:
-            var test = (mode_: string): boolean => test_(this.createHTMLDocument_, mode = mode_);
-            switch (/webkit|firefox|trident|$/i.exec(window.navigator.userAgent.toLowerCase()).shift()) {
-              case 'webkit':
-                test('doc') || test('dom') || test('manipulate');
-                break;
-              case 'firefox':
-                test('dom') || test('doc') || test('manipulate');
-                break;
-              case 'trident':
-                test('manipulate') || test('dom') || test('doc');
-                break;
-              default:
-                test('dom') || test('doc') || test('manipulate');
-            }
-            doc = this.createHTMLDocument_(html, uri);
-            break;
-        }
-
-        uri && window.history.replaceState(window.history.state, document.title, backup);
-        return doc;
-      };
-
-      return this.createHTMLDocument_(html, uri);
-    }
-
   }
 
 }
--- src/ts/model/app.page.utility.ts
+++ src/ts/model/app.page.utility.ts
@@ -4,7 +4,112 @@
 
 module MODULE.MODEL {
 
+  var mode: string;
+
   export class AppPageUtility implements AppPageUtilityInterface {
+    
+    createHTMLDocument(html: string, uri: string): Document {
+      var test = (mode_: string): boolean => {
+        try {
+          mode = mode_;
+          var html = '<html lang="en" class="html"><head><title>&amp;</title><link href="/"><noscript><style>/**/</style></noscript></head><body><noscript>noscript</noscript><a href="/"></a></body></html>',
+              doc = this.createHTMLDocument(html, '');
+          switch (false) {
+            case !!doc:
+            case doc.URL && decodeURI(doc.URL) === decodeURI(uri || window.location.href):
+            case doc.title === '&':
+            case !!doc.querySelector('html.html[lang="en"]'):
+            case !!doc.querySelector('head>link')['href']:
+            case !!doc.querySelector('head>noscript')['innerHTML']:
+            case !!doc.querySelector('body>noscript')['innerHTML']:
+            case !!doc.querySelector('body>a')['href']:
+              throw true;
+          }
+          return true;
+        } catch (err) {
+          mode = null;
+          return false;
+        }
+      };
+      function manipulate(doc: Document, html: string): Document {
+        var wrapper = <HTMLElement>document.createElement('div');
+        wrapper.innerHTML = html.match(/<html(?:\s.*?[^\\])?>|$/i).shift().replace(/html/i, 'div') || '<div>';
+        var attrs = wrapper.firstChild.attributes;
+        for (var i = 0, attr: Attr; attr = attrs[i]; i++) {
+          doc.documentElement.setAttribute(attr.name, attr.value);
+        }
+        var wrapper = <HTMLElement>document.createElement('html');
+        wrapper.innerHTML = html.replace(/^.*?<html(?:\s.*?[^\\])?>/im, '');
+        doc.documentElement.removeChild(doc.head);
+        doc.documentElement.removeChild(doc.body);
+        while (wrapper.childNodes.length) {
+          doc.documentElement.appendChild(wrapper.childNodes[0]);
+        }
+        return doc;
+      };
+
+      var backup = window.location.href;
+      uri && window.history.replaceState(window.history.state, document.title, uri);
+
+      var doc: Document;
+      switch (mode) {
+        // firefox
+        case 'dom':
+          if ('function' === typeof window.DOMParser) {
+            doc = new window.DOMParser().parseFromString(html, 'text/html');
+          }
+          break;
+
+        // chrome, safari
+        case 'doc':
+          if (document.implementation && document.implementation.createHTMLDocument) {
+            doc = document.implementation.createHTMLDocument('');
+
+            // IE, Operaクラッシュ対策
+            if ('object' !== typeof doc.activeElement || !doc.activeElement) { break; }
+
+            // titleプロパティの値をChromeで事後に変更できなくなったため事前に設定する必要がある
+            if ('function' === typeof window.DOMParser) {
+              doc.title = new window.DOMParser().parseFromString(html.match(/<title(?:\s.*?[^\\])?>(?:.*?[^\\])?<\/title>/i), 'text/html').title;
+            }
+            doc.open();
+            doc.write(html);
+            doc.close();
+          }
+          break;
+
+        // ie10+, opera
+        case 'manipulate':
+          if (document.implementation && document.implementation.createHTMLDocument) {
+            doc = manipulate(document.implementation.createHTMLDocument(''), html);
+          }
+          break;
+
+        case null:
+          doc = null;
+          break;
+
+        default:
+          switch (/webkit|firefox|trident|$/i.exec(window.navigator.userAgent.toLowerCase()).shift()) {
+            case 'webkit':
+              test('doc') || test('dom') || test('manipulate');
+              break;
+            case 'firefox':
+              test('dom') || test('doc') || test('manipulate');
+              break;
+            case 'trident':
+              test('manipulate') || test('dom') || test('doc');
+              break;
+            default:
+              test('dom') || test('doc') || test('manipulate');
+          }
+          doc = this.createHTMLDocument(html, uri);
+          break;
+      }
+
+      uri && window.history.replaceState(window.history.state, document.title, backup);
+      return doc;
+    }
 
     chooseArea(area: string, srcDocument: Document, dstDocument: Document): string
     chooseArea(areas: string[], srcDocument: Document, dstDocument: Document): string
