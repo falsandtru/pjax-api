269f7592f2d8387315c21fe9ee69b82573a6aed8
falsandtru <falsandtru@users.noreply.github.com>
2016-10-04 03:07:09 +0900

    fix(interface): fix the initial script detection

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 3.3.3
+
+- Fix the initial script detection.
+
 ## 3.3.2
 
 - Update dependencies.
--- src/layer/interface/service/state/script.ts
+++ src/layer/interface/service/state/script.ts
@@ -1,11 +1,11 @@
-import { Tick, concat } from 'spica';
+import { concat } from 'spica';
 import { canonicalizeUrl, CanonicalUrl } from '../../../data/model/canonicalization/url';
 import { validateUrl } from '../../../data/model/validation/url';
 import { find } from '../../../../lib/dom';
 
 export const script: CanonicalUrl[] = [];
 
-void Tick(() =>
+void setTimeout(() =>
   void concat(script, find<HTMLScriptElement>(document, 'script')
     .filter(script => script.hasAttribute('src'))
     .map(script => canonicalizeUrl(validateUrl(script.src)))));
