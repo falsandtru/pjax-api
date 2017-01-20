e4f526b2204264f2c2fbc9dba8f58a0e55d27ff6
falsandtru <falsandtru@users.noreply.github.com>
2017-01-20 17:27:34 +0900

    refactor(global): remove unused workarounds

--- global.d.ts
+++ global.d.ts
@@ -4,14 +4,6 @@ import _assert from 'power-assert';
 declare global {
   export const assert: typeof _assert;
 
-  interface PromiseLike<T> {
-    _?: T;
-  }
-
-  interface Promise<T> {
-    _?: T;
-  }
-
   interface Event {
     _currentTarget: HTMLElement;
   }
