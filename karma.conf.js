301149a14576fa5a072264e4b06c639f5b19e1c2
falsandtru <falsandtru@users.noreply.github.com>
2017-01-28 17:55:43 +0900

    chore(config): fix build scripts

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['mocha'],
     files: [
-      { pattern: 'https://cdn.polyfill.io/v2/polyfill.js?features=default,WeakMap,WeakSet&flags=gated', watched: false, served: false, included: true },
+      { pattern: 'https://cdn.polyfill.io/v2/polyfill.js?features=default,WeakMap,WeakSet,Array.prototype.findIndex,Array.prototype.@@iterator&flags=gated', watched: false, served: false, included: true },
       { pattern: 'node_modules/power-assert/build/power-assert.js', watched: true, served: true, included: true },
       { pattern: 'node_modules/typed-dom/dist/typed-dom.js', watched: true, served: true, included: true },
       { pattern: 'node_modules/spica/dist/spica.js', watched: true, served: true, included: true },
