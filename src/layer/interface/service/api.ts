d3b24747b0708a36250702f3baeebfda30a6008e
falsandtru <falsandtru@users.noreply.github.com>
2017-07-15 13:58:17 +0900

    refactor(interface): refactoring

--- src/layer/interface/service/api.ts
+++ src/layer/interface/service/api.ts
@@ -9,25 +9,19 @@ import { parse } from '../../../lib/html';
 
 export class API {
   public static assign(url: string, option: Option, io = { document: window.document }): undefined {
-    return void click(url)
-      .then(event =>
-        route(new Config(option), event, process, io));
+    return void click(url, event =>
+      void route(new Config(option), event, process, io));
   }
   public static replace(url: string, option: Option, io = { document: window.document }): undefined {
-    return void click(url)
-      .then(event =>
-        route(new Config(extend<Option>({}, option, { replace: '*' })), event, process, io));
+    return void click(url, event =>
+      void route(new Config(extend<Option>({}, option, { replace: '*' })), event, process, io));
   }
 }
 
-function click(url: string): Promise<Event> {
+function click(url: string, callback: (ev: Event) => void): void {
   const el: RouterEventSource.Anchor = document.createElement('a');
   el.href = url;
-  return new Promise<Event>(resolve => (
-    void once(el, 'click', event => (
-      void event.preventDefault(),
-      void resolve(event))),
-    void parse('').extract().body.appendChild(el),
-    void el.click(),
-    void el.remove()));
+  void parse('').extract().body.appendChild(el);
+  void once(el, 'click', callback);
+  void el.click();
 }
