91d2321ecd0689b1a48ae71be59dca8754e2f1c3
falsandtru <falsandtru@users.noreply.github.com>
2017-08-13 19:18:01 +0900

    chore(site): fix polyfill settings

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['mocha'],
     files: [
-      { pattern: 'https://cdn.polyfill.io/v2/polyfill.js?features=default,WeakMap,WeakSet,Array.prototype.findIndex,Array.prototype.@@iterator&flags=gated', watched: false, served: false, included: true },
+      { pattern: 'https://cdn.polyfill.io/v2/polyfill.js?features=default,WeakMap,WeakSet,Array.prototype.findIndex,Array.prototype.@@iterator,String.prototype.repeat&flags=gated', watched: false, served: false, included: true },
       { pattern: 'node_modules/power-assert/build/power-assert.js', watched: true, served: true, included: true },
       { pattern: 'dist/*.js', watched: true, served: true, included: true },
       { pattern: 'test/integration/usecase/**/*.{html,css,js}', watched: true, served: true, included: true },
