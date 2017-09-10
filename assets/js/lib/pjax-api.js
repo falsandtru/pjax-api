a997c1d6253138ae42b7bcb567ac835247dbf81b
Travis-CI <Travis-CI>
2017-09-10 07:27:53 +0000

    chore(deploy): deploy web contents by Travis-CI

--- assets/js/lib/pjax-api.js
+++ assets/js/lib/pjax-api.js
@@ -1,4 +1,4 @@
-/*! pjax-api v3.15.1 https://github.com/falsandtru/pjax-api | (c) 2016, falsandtru | (Apache-2.0 AND MPL-2.0) License */
+/*! pjax-api v3.15.2 https://github.com/falsandtru/pjax-api | (c) 2016, falsandtru | (Apache-2.0 AND MPL-2.0) License */
 require = function e(t, n, r) {
     function s(o, u) {
         if (!n[o]) {
