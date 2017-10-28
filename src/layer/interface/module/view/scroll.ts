f74eba31ebbd4df49d8ea459cb1ec89da16ad607
falsandtru <falsandtru@users.noreply.github.com>
2017-10-28 09:14:17 +0900

    refactor(interface): refactoring

--- src/layer/interface/module/view/scroll.ts
+++ src/layer/interface/module/view/scroll.ts
@@ -1,28 +1,27 @@
 import { Supervisor } from 'spica/supervisor';
 import { bind } from 'typed-dom';
+import { throttle } from 'spica/throttle';
 
 export class ScrollView {
   constructor(
     window: Window,
     listener: (event: Event) => void,
   ) {
-    let timer = 0;
     void this.sv.register('', () => (
       void this.sv.events.exit.monitor(
         [],
-        bind(window, 'scroll', ev => (
-          timer = timer > 0
-            ? timer
-            : setTimeout(() => {
-              timer = 0;
-              void listener(ev);
-            }, 300)
-        ), { passive: true })),
+        bind(window, 'scroll', throttle<UIEvent>(300, ev => (
+          this.active &&
+          void listener(ev)
+        )), { passive: true })),
       new Promise<never>(() => undefined)
     ), undefined);
     void this.sv.cast('', undefined);
   }
   private readonly sv = new class extends Supervisor<'', void, void, void>{ }();
-  public readonly close = () =>
-    void this.sv.terminate();
+  private active = true;
+  public readonly close = () => (
+    this.active = false,
+    void this.sv.terminate()
+  );
 }
