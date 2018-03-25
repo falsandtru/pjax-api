3c79cc47194907ed88b73395d5c4308de6542621
falsandtru <falsandtru@users.noreply.github.com>
2018-03-25 22:41:05 +0900

    Refactoring

--- src/layer/interface/service/state/script.ts
+++ src/layer/interface/service/state/script.ts
@@ -1,8 +1,7 @@
 import { StandardUrl, standardizeUrl } from '../../../data/model/domain/url';
 import { URL } from '../../../../lib/url';
 import { find } from '../../../../lib/dom';
-import { bind, once } from 'typed-dom';
-import { concat } from 'spica/concat';
+import { bind } from 'typed-dom';
 
 export const scripts = new Set<URL.Absolute<StandardUrl>>();
 
@@ -10,20 +9,3 @@ void bind(window, 'pjax:unload', () =>
   void find<HTMLScriptElement>(document, 'script[src]')
     .forEach(script =>
       void scripts.add(new URL(standardizeUrl(script.src)).href)));
-
-void new MutationObserver(ms =>
-  void ms
-    .reduce<HTMLScriptElement[]>((ss, { addedNodes }) =>
-      concat(
-        ss,
-        [...addedNodes]
-          .filter((node): node is HTMLScriptElement =>
-            node instanceof HTMLScriptElement && node.hasAttribute('src')))
-    , [])
-    .forEach(script =>
-      void once(script, 'load', () =>
-        void scripts.add(new URL(standardizeUrl(script.src)).href))))
-  .observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-  });
