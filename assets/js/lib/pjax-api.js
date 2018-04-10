647f59845c188f62080ae7d45ae0b3ef98cd1d66
Travis-CI <Travis-CI>
2018-04-10 13:08:51 +0000

    chore: update web contents

--- assets/js/lib/pjax-api.js
+++ assets/js/lib/pjax-api.js
@@ -1,31 +1,30 @@
 /*! pjax-api v3.21.2 https://github.com/falsandtru/pjax-api | (c) 2012, falsandtru | (Apache-2.0 AND MPL-2.0) License */
 require = function () {
-    function e(t, n, r) {
-        function s(o, u) {
-            if (!n[o]) {
-                if (!t[o]) {
-                    var a = typeof require == 'function' && require;
-                    if (!u && a)
-                        return a(o, !0);
-                    if (i)
-                        return i(o, !0);
-                    var f = new Error('Cannot find module \'' + o + '\'');
-                    throw f.code = 'MODULE_NOT_FOUND', f;
+    function r(e, n, t) {
+        function o(i, f) {
+            if (!n[i]) {
+                if (!e[i]) {
+                    var c = 'function' == typeof require && require;
+                    if (!f && c)
+                        return c(i, !0);
+                    if (u)
+                        return u(i, !0);
+                    var a = new Error('Cannot find module \'' + i + '\'');
+                    throw a.code = 'MODULE_NOT_FOUND', a;
                 }
-                var l = n[o] = { exports: {} };
-                t[o][0].call(l.exports, function (e) {
-                    var n = t[o][1][e];
-                    return s(n ? n : e);
-                }, l, l.exports, e, t, n, r);
+                var p = n[i] = { exports: {} };
+                e[i][0].call(p.exports, function (r) {
+                    var n = e[i][1][r];
+                    return o(n || r);
+                }, p, p.exports, r, e, n, t);
             }
-            return n[o].exports;
+            return n[i].exports;
         }
-        var i = typeof require == 'function' && require;
-        for (var o = 0; o < r.length; o++)
-            s(r[o]);
-        return s;
+        for (var u = 'function' == typeof require && require, i = 0; i < t.length; i++)
+            o(t[i]);
+        return o;
     }
-    return e;
+    return r;
 }()({
     1: [
         function (require, module, exports) {
@@ -2967,14 +2966,14 @@ require = function () {
             }
             exports.svg = svg;
             function element(ns, tag, attrs = {}, children = []) {
+                if (isChildren(attrs))
+                    return element(ns, tag, {}, attrs);
                 if (typeof children === 'string')
                     return element(ns, tag, attrs, [document.createTextNode(children)]);
-                if (typeof attrs === 'string' || Array.isArray(attrs))
-                    return element(ns, tag, {}, attrs);
                 const key = `${ ns }:${ tag }`;
                 const el = cache.has(key) ? cache.get(key).cloneNode(true) : cache.set(key, elem(ns, tag)).get(key).cloneNode(true);
                 void Object.entries(attrs).forEach(([name, value]) => void el.setAttribute(name, value));
-                void children.forEach(child => void el.appendChild(child));
+                void [...children].forEach(child => void el.appendChild(child));
                 return el;
             }
             function elem(ns, tag) {
@@ -2986,6 +2985,9 @@ require = function () {
                 default:
                     throw new Error(`TypedDOM: Unknown namespace: ${ ns }`);
                 }
+            }
+            function isChildren(o) {
+                return !!o[Symbol.iterator];
             }
         },
         {}
