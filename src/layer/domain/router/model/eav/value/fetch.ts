94dc9da20c6dab9955d6c34384c985074b9966e4
falsandtru <falsandtru@users.noreply.github.com>
2018-04-28 12:12:50 +0900

    refactor: refactoring

--- src/layer/domain/router/model/eav/value/fetch.ts
+++ src/layer/domain/router/model/eav/value/fetch.ts
@@ -1,4 +1,5 @@
 import { StandardUrl } from '../../../../../data/model/domain/url';
+import { fix } from '../../../../../../lib/html';
 
 export class FetchResponse {
   constructor(
@@ -7,10 +8,11 @@ export class FetchResponse {
   ) {
     assert(this.xhr.responseType === 'document');
     assert(this.document instanceof Document);
+    void fix(this.document);
     void Object.freeze(this);
   }
   public readonly header: (name: string) => string | null =
     name => this.xhr.getResponseHeader(name);
   public readonly document: Document =
-    this.xhr.responseXML!;
+    this.xhr.response;
 }
