e4352d21b8404e18afef4d3fd73835d996b6c294
falsandtru <falsandtru@users.noreply.github.com>
2018-11-09 09:00:39 +0900

    refactor: refactoring

--- src/layer/interface/service/state/url.ts
+++ src/layer/interface/service/state/url.ts
@@ -1,16 +1,15 @@
 import { StandardUrl, standardizeUrl } from '../../../data/model/domain/url';
 import { bind } from 'typed-dom';
 
-let url = standardizeUrl(location.href);
-
 void bind(window, 'hashchange', () =>
   void docurl.sync());
 
 export const docurl = new class {
+  private url: StandardUrl = standardizeUrl(location.href);
   public get href(): StandardUrl {
-    return url;
+    return this.url;
   }
   public sync(): void {
-    url = standardizeUrl(location.href);
+    this.url = standardizeUrl(location.href);
   }
-}();
+};
