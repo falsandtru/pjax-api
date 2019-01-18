ffc88fd37333e8f2a737b4cdf9f77e257bf49d67
Travis-CI <Travis-CI>
2019-01-18 15:52:57 +0000

    chore: update web contents

--- assets/js/lib/pjax-api.js
+++ assets/js/lib/pjax-api.js
@@ -16062,7 +16062,7 @@ require = function () {
                 this.seq().obj(this.key('type').objid(), this.key('value').any());
             });
             var AlgorithmIdentifier = asn.define('AlgorithmIdentifier', function () {
-                this.seq().obj(this.key('algorithm').objid(), this.key('parameters').optional());
+                this.seq().obj(this.key('algorithm').objid(), this.key('parameters').optional(), this.key('curve').objid().optional());
             });
             var SubjectPublicKeyInfo = asn.define('SubjectPublicKeyInfo', function () {
                 this.seq().obj(this.key('algorithm').use(AlgorithmIdentifier), this.key('subjectPublicKey').bitstr());
@@ -16083,7 +16083,7 @@ require = function () {
                 this.seq().obj(this.key('extnID').objid(), this.key('critical').bool().def(false), this.key('extnValue').octstr());
             });
             var TBSCertificate = asn.define('TBSCertificate', function () {
-                this.seq().obj(this.key('version').explicit(0).int(), this.key('serialNumber').int(), this.key('signature').use(AlgorithmIdentifier), this.key('issuer').use(Name), this.key('validity').use(Validity), this.key('subject').use(Name), this.key('subjectPublicKeyInfo').use(SubjectPublicKeyInfo), this.key('issuerUniqueID').implicit(1).bitstr().optional(), this.key('subjectUniqueID').implicit(2).bitstr().optional(), this.key('extensions').explicit(3).seqof(Extension).optional());
+                this.seq().obj(this.key('version').explicit(0).int().optional(), this.key('serialNumber').int(), this.key('signature').use(AlgorithmIdentifier), this.key('issuer').use(Name), this.key('validity').use(Validity), this.key('subject').use(Name), this.key('subjectPublicKeyInfo').use(SubjectPublicKeyInfo), this.key('issuerUniqueID').implicit(1).bitstr().optional(), this.key('subjectUniqueID').implicit(2).bitstr().optional(), this.key('extensions').explicit(3).seqof(Extension).optional());
             });
             var X509Certificate = asn.define('X509Certificate', function () {
                 this.seq().obj(this.key('tbsCertificate').use(TBSCertificate), this.key('signatureAlgorithm').use(AlgorithmIdentifier), this.key('signatureValue').bitstr());
@@ -16134,120 +16134,119 @@ require = function () {
     ],
     114: [
         function (require, module, exports) {
-            (function (Buffer) {
-                var asn1 = require('./asn1');
-                var aesid = require('./aesid.json');
-                var fixProc = require('./fixProc');
-                var ciphers = require('browserify-aes');
-                var compat = require('pbkdf2');
-                module.exports = parseKeys;
-                function parseKeys(buffer) {
-                    var password;
-                    if (typeof buffer === 'object' && !Buffer.isBuffer(buffer)) {
-                        password = buffer.passphrase;
-                        buffer = buffer.key;
+            var asn1 = require('./asn1');
+            var aesid = require('./aesid.json');
+            var fixProc = require('./fixProc');
+            var ciphers = require('browserify-aes');
+            var compat = require('pbkdf2');
+            var Buffer = require('safe-buffer').Buffer;
+            module.exports = parseKeys;
+            function parseKeys(buffer) {
+                var password;
+                if (typeof buffer === 'object' && !Buffer.isBuffer(buffer)) {
+                    password = buffer.passphrase;
+                    buffer = buffer.key;
+                }
+                if (typeof buffer === 'string') {
+                    buffer = Buffer.from(buffer);
+                }
+                var stripped = fixProc(buffer, password);
+                var type = stripped.tag;
+                var data = stripped.data;
+                var subtype, ndata;
+                switch (type) {
+                case 'CERTIFICATE':
+                    ndata = asn1.certificate.decode(data, 'der').tbsCertificate.subjectPublicKeyInfo;
+                case 'PUBLIC KEY':
+                    if (!ndata) {
+                        ndata = asn1.PublicKey.decode(data, 'der');
                     }
-                    if (typeof buffer === 'string') {
-                        buffer = new Buffer(buffer);
-                    }
-                    var stripped = fixProc(buffer, password);
-                    var type = stripped.tag;
-                    var data = stripped.data;
-                    var subtype, ndata;
-                    switch (type) {
-                    case 'CERTIFICATE':
-                        ndata = asn1.certificate.decode(data, 'der').tbsCertificate.subjectPublicKeyInfo;
-                    case 'PUBLIC KEY':
-                        if (!ndata) {
-                            ndata = asn1.PublicKey.decode(data, 'der');
-                        }
-                        subtype = ndata.algorithm.algorithm.join('.');
-                        switch (subtype) {
-                        case '1.2.840.113549.1.1.1':
-                            return asn1.RSAPublicKey.decode(ndata.subjectPublicKey.data, 'der');
-                        case '1.2.840.10045.2.1':
-                            ndata.subjectPrivateKey = ndata.subjectPublicKey;
-                            return {
-                                type: 'ec',
-                                data: ndata
-                            };
-                        case '1.2.840.10040.4.1':
-                            ndata.algorithm.params.pub_key = asn1.DSAparam.decode(ndata.subjectPublicKey.data, 'der');
-                            return {
-                                type: 'dsa',
-                                data: ndata.algorithm.params
-                            };
-                        default:
-                            throw new Error('unknown key id ' + subtype);
-                        }
-                        throw new Error('unknown key type ' + type);
-                    case 'ENCRYPTED PRIVATE KEY':
-                        data = asn1.EncryptedPrivateKey.decode(data, 'der');
-                        data = decrypt(data, password);
-                    case 'PRIVATE KEY':
-                        ndata = asn1.PrivateKey.decode(data, 'der');
-                        subtype = ndata.algorithm.algorithm.join('.');
-                        switch (subtype) {
-                        case '1.2.840.113549.1.1.1':
-                            return asn1.RSAPrivateKey.decode(ndata.subjectPrivateKey, 'der');
-                        case '1.2.840.10045.2.1':
-                            return {
-                                curve: ndata.algorithm.curve,
-                                privateKey: asn1.ECPrivateKey.decode(ndata.subjectPrivateKey, 'der').privateKey
-                            };
-                        case '1.2.840.10040.4.1':
-                            ndata.algorithm.params.priv_key = asn1.DSAparam.decode(ndata.subjectPrivateKey, 'der');
-                            return {
-                                type: 'dsa',
-                                params: ndata.algorithm.params
-                            };
-                        default:
-                            throw new Error('unknown key id ' + subtype);
-                        }
-                        throw new Error('unknown key type ' + type);
-                    case 'RSA PUBLIC KEY':
-                        return asn1.RSAPublicKey.decode(data, 'der');
-                    case 'RSA PRIVATE KEY':
-                        return asn1.RSAPrivateKey.decode(data, 'der');
-                    case 'DSA PRIVATE KEY':
+                    subtype = ndata.algorithm.algorithm.join('.');
+                    switch (subtype) {
+                    case '1.2.840.113549.1.1.1':
+                        return asn1.RSAPublicKey.decode(ndata.subjectPublicKey.data, 'der');
+                    case '1.2.840.10045.2.1':
+                        ndata.subjectPrivateKey = ndata.subjectPublicKey;
+                        return {
+                            type: 'ec',
+                            data: ndata
+                        };
+                    case '1.2.840.10040.4.1':
+                        ndata.algorithm.params.pub_key = asn1.DSAparam.decode(ndata.subjectPublicKey.data, 'der');
                         return {
                             type: 'dsa',
-                            params: asn1.DSAPrivateKey.decode(data, 'der')
-                        };
-                    case 'EC PRIVATE KEY':
-                        data = asn1.ECPrivateKey.decode(data, 'der');
-                        return {
-                            curve: data.parameters.value,
-                            privateKey: data.privateKey
+                            data: ndata.algorithm.params
                         };
                     default:
-                        throw new Error('unknown key type ' + type);
+                        throw new Error('unknown key id ' + subtype);
                     }
+                    throw new Error('unknown key type ' + type);
+                case 'ENCRYPTED PRIVATE KEY':
+                    data = asn1.EncryptedPrivateKey.decode(data, 'der');
+                    data = decrypt(data, password);
+                case 'PRIVATE KEY':
+                    ndata = asn1.PrivateKey.decode(data, 'der');
+                    subtype = ndata.algorithm.algorithm.join('.');
+                    switch (subtype) {
+                    case '1.2.840.113549.1.1.1':
+                        return asn1.RSAPrivateKey.decode(ndata.subjectPrivateKey, 'der');
+                    case '1.2.840.10045.2.1':
+                        return {
+                            curve: ndata.algorithm.curve,
+                            privateKey: asn1.ECPrivateKey.decode(ndata.subjectPrivateKey, 'der').privateKey
+                        };
+                    case '1.2.840.10040.4.1':
+                        ndata.algorithm.params.priv_key = asn1.DSAparam.decode(ndata.subjectPrivateKey, 'der');
+                        return {
+                            type: 'dsa',
+                            params: ndata.algorithm.params
+                        };
+                    default:
+                        throw new Error('unknown key id ' + subtype);
+                    }
+                    throw new Error('unknown key type ' + type);
+                case 'RSA PUBLIC KEY':
+                    return asn1.RSAPublicKey.decode(data, 'der');
+                case 'RSA PRIVATE KEY':
+                    return asn1.RSAPrivateKey.decode(data, 'der');
+                case 'DSA PRIVATE KEY':
+                    return {
+                        type: 'dsa',
+                        params: asn1.DSAPrivateKey.decode(data, 'der')
+                    };
+                case 'EC PRIVATE KEY':
+                    data = asn1.ECPrivateKey.decode(data, 'der');
+                    return {
+                        curve: data.parameters.value,
+                        privateKey: data.privateKey
+                    };
+                default:
+                    throw new Error('unknown key type ' + type);
                 }
-                parseKeys.signature = asn1.signature;
-                function decrypt(data, password) {
-                    var salt = data.algorithm.decrypt.kde.kdeparams.salt;
-                    var iters = parseInt(data.algorithm.decrypt.kde.kdeparams.iters.toString(), 10);
-                    var algo = aesid[data.algorithm.decrypt.cipher.algo.join('.')];
-                    var iv = data.algorithm.decrypt.cipher.iv;
-                    var cipherText = data.subjectPrivateKey;
-                    var keylen = parseInt(algo.split('-')[1], 10) / 8;
-                    var key = compat.pbkdf2Sync(password, salt, iters, keylen);
-                    var cipher = ciphers.createDecipheriv(algo, key, iv);
-                    var out = [];
-                    out.push(cipher.update(cipherText));
-                    out.push(cipher.final());
-                    return Buffer.concat(out);
-                }
-            }.call(this, require('buffer').Buffer));
+            }
+            parseKeys.signature = asn1.signature;
+            function decrypt(data, password) {
+                var salt = data.algorithm.decrypt.kde.kdeparams.salt;
+                var iters = parseInt(data.algorithm.decrypt.kde.kdeparams.iters.toString(), 10);
+                var algo = aesid[data.algorithm.decrypt.cipher.algo.join('.')];
+                var iv = data.algorithm.decrypt.cipher.iv;
+                var cipherText = data.subjectPrivateKey;
+                var keylen = parseInt(algo.split('-')[1], 10) / 8;
+                var key = compat.pbkdf2Sync(password, salt, iters, keylen, 'sha1');
+                var cipher = ciphers.createDecipheriv(algo, key, iv);
+                var out = [];
+                out.push(cipher.update(cipherText));
+                out.push(cipher.final());
+                return Buffer.concat(out);
+            }
         },
         {
             './aesid.json': 110,
             './asn1': 111,
             './fixProc': 113,
             'browserify-aes': 24,
-            'buffer': 50,
-            'pbkdf2': 115
+            'pbkdf2': 115,
+            'safe-buffer': 145
         }
     ],
     115: [
