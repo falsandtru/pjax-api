0f0528a566d19a78f4193a12100e88bf4f897d80
Travis-CI <Travis-CI>
2019-02-16 02:31:22 +0000

    chore: update web contents

--- assets/js/lib/pjax-api.js
+++ assets/js/lib/pjax-api.js
@@ -16094,42 +16094,41 @@ require = function () {
     ],
     113: [
         function (require, module, exports) {
-            (function (Buffer) {
-                var findProc = /Proc-Type: 4,ENCRYPTED[\n\r]+DEK-Info: AES-((?:128)|(?:192)|(?:256))-CBC,([0-9A-H]+)[\n\r]+([0-9A-z\n\r\+\/\=]+)[\n\r]+/m;
-                var startRegex = /^-----BEGIN ((?:.* KEY)|CERTIFICATE)-----/m;
-                var fullRegex = /^-----BEGIN ((?:.* KEY)|CERTIFICATE)-----([0-9A-z\n\r\+\/\=]+)-----END \1-----$/m;
-                var evp = require('evp_bytestokey');
-                var ciphers = require('browserify-aes');
-                module.exports = function (okey, password) {
-                    var key = okey.toString();
-                    var match = key.match(findProc);
-                    var decrypted;
-                    if (!match) {
-                        var match2 = key.match(fullRegex);
-                        decrypted = new Buffer(match2[2].replace(/[\r\n]/g, ''), 'base64');
-                    } else {
-                        var suite = 'aes' + match[1];
-                        var iv = new Buffer(match[2], 'hex');
-                        var cipherText = new Buffer(match[3].replace(/[\r\n]/g, ''), 'base64');
-                        var cipherKey = evp(password, iv.slice(0, 8), parseInt(match[1], 10)).key;
-                        var out = [];
-                        var cipher = ciphers.createDecipheriv(suite, cipherKey, iv);
-                        out.push(cipher.update(cipherText));
-                        out.push(cipher.final());
-                        decrypted = Buffer.concat(out);
-                    }
-                    var tag = key.match(startRegex)[1];
-                    return {
-                        tag: tag,
-                        data: decrypted
-                    };
+            var findProc = /Proc-Type: 4,ENCRYPTED[\n\r]+DEK-Info: AES-((?:128)|(?:192)|(?:256))-CBC,([0-9A-H]+)[\n\r]+([0-9A-z\n\r\+\/\=]+)[\n\r]+/m;
+            var startRegex = /^-----BEGIN ((?:.*? KEY)|CERTIFICATE)-----/m;
+            var fullRegex = /^-----BEGIN ((?:.*? KEY)|CERTIFICATE)-----([0-9A-z\n\r\+\/\=]+)-----END \1-----$/m;
+            var evp = require('evp_bytestokey');
+            var ciphers = require('browserify-aes');
+            var Buffer = require('safe-buffer').Buffer;
+            module.exports = function (okey, password) {
+                var key = okey.toString();
+                var match = key.match(findProc);
+                var decrypted;
+                if (!match) {
+                    var match2 = key.match(fullRegex);
+                    decrypted = new Buffer(match2[2].replace(/[\r\n]/g, ''), 'base64');
+                } else {
+                    var suite = 'aes' + match[1];
+                    var iv = Buffer.from(match[2], 'hex');
+                    var cipherText = Buffer.from(match[3].replace(/[\r\n]/g, ''), 'base64');
+                    var cipherKey = evp(password, iv.slice(0, 8), parseInt(match[1], 10)).key;
+                    var out = [];
+                    var cipher = ciphers.createDecipheriv(suite, cipherKey, iv);
+                    out.push(cipher.update(cipherText));
+                    out.push(cipher.final());
+                    decrypted = Buffer.concat(out);
+                }
+                var tag = key.match(startRegex)[1];
+                return {
+                    tag: tag,
+                    data: decrypted
                 };
-            }.call(this, require('buffer').Buffer));
+            };
         },
         {
             'browserify-aes': 24,
-            'buffer': 50,
-            'evp_bytestokey': 86
+            'evp_bytestokey': 86,
+            'safe-buffer': 145
         }
     ],
     114: [
