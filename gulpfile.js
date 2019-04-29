7781b3cb141ab5bc48fd5e1ba50006603c5edaeb
falsandtru <falsandtru@users.noreply.github.com>
2019-04-29 20:45:02 +0900

    chore: update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -160,7 +160,7 @@ gulp.task('install', done => {
 
 gulp.task('update', done => {
   shell('bundle update');
-  shell('ncu -u');
+  shell('ncu -ux del');
   shell('npm i -DE typescript@next --no-shrinkwrap');
   shell('npm i --no-shrinkwrap');
   done();
