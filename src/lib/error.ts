7adab39aab0a515341f4cd0d6dd3058cbb197b92
falsandtru <falsandtru@users.noreply.github.com>
2019-10-06 11:57:51 +0900

    refactor: refactoring

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -1,9 +1,10 @@
 export class FatalError extends Error {
   constructor(msg: string) {
     super(msg);
-    this.name = 'FatalError';
   }
 }
+Error.prototype.name = 'Error';
+FatalError.prototype.name = 'FatalError';
 assert(new FatalError('') instanceof Error);
 assert(new FatalError('') instanceof FatalError);
 assert(new FatalError('').name === 'FatalError');
