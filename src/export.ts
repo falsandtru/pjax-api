7b00d17cbbc7196a75a7ae3e940dac0a9930cb46
falsandtru <falsandtru@users.noreply.github.com>
2020-03-06 03:14:54 +0900

    Refactoring

--- src/export.ts
+++ src/export.ts
@@ -1,3 +1,2 @@
-export { GUI as default } from './layer/interface/service/gui';
-export { GUI as Pjax } from './layer/interface/service/gui';
+export { GUI as Pjax, GUI as default } from './layer/interface/service/gui';
 export { router } from './lib/router';
