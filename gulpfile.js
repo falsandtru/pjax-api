2d8ec94060d4ca0c18b09d5d335dfeb705ad39e4
falsandtru <falsandtru@users.noreply.github.com>
2020-10-12 03:28:46 +0900

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -158,7 +158,7 @@ gulp.task('install', done => {
 gulp.task('update', done => {
   shell('bundle update');
   shell('ncu -ux del');
-  shell('ncu -ut typescript');
+  shell('ncu -ut greatest typescript');
   shell('npm i --no-shrinkwrap');
   done();
 });
