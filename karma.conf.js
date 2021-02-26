88177c13fe64c2cf20bacff6f209fbfdad59b48b
falsandtru <falsandtru@users.noreply.github.com>
2021-02-26 22:28:23 +0900

    Refactoring

--- karma.conf.js
+++ karma.conf.js
@@ -38,6 +38,7 @@ module.exports = function (config) {
     },
     autoWatch: true,
     autoWatchBatchDelay: 500,
+    browserDisconnectTimeout: 30000,
     browsers: ['Chrome'],
     singleRun: true,
   });
